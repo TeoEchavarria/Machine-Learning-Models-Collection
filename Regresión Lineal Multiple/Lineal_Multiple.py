66323dfce6c9c97dba46f26604eecc2529d44352
Mateo Echavarria Sierra <63327224+TeoEchavarria@users.noreply.github.com>
2023-01-15 18:00:11 -0500

    Update Lineal_Multiple.py
    - Añado código para la visualización de los datos

--- Regresión Lineal Multiple/Lineal_Multiple.py
+++ Regresión Lineal Multiple/Lineal_Multiple.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct 23 19:02:12 2022
+Created on Enero 2023
 
-@author: Mateo
+@author: Mateo Echavarria Sierra
 """
 
 import numpy as np
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
  
 # Importar el Data set
-dataset = pd.read_csv("Data/50_Startups.csv")
+dataset = pd.read_csv("50_Startups.csv")
 X = dataset.iloc[:, :-1].values 
 y = dataset.iloc[:, 4].values  # variable a predecir el profit
 
@@ -104,13 +104,33 @@ error_aprox = np.mean([ abs((y_prediction_optimal[i]-y_test[i])/y_test[i]) for i
 print(f' Error aproximado entre y_prediction_optimal & y_test = {round(error_aprox, 4)*100} %')
 
 #Visualización de datos
-fig = plt.figure()
-ax1 = fig.add_subplot(111, projection='3d')
-
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(1,1,1, projection='3d')
 
 # Agregamos los puntos en el plano 3D
-ax1.scatter(X_train[:, 1], X_train[:, 2], y_train, c='g', marker='o')
+
+ax1.scatter(X_train[:, 1], X_train[:, 2], y_train, c='g')
 plot_y = sorted([i for i in regression.predict(X_train)])
 ax1.plot(sorted(X_train[:, 1]), sorted(X_train[:, 2]), plot_y, c="r")
-# Mostramos el gráfico
+plt.title("Conjunto de Entrenamiento", )
+ax1.set_xlabel('R&D spend', fontsize=10, ha="right", fontweight="regular")
+ax1.set_ylabel('Gastos en Marketing', fontsize=10)
+ax1.set_zlabel('Beneficios', fontsize=10)
+
+
+
+plt.show()
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111, projection='3d')
+
+# Agregamos los puntos en el plano 3D
+ax2.scatter(X_test[:, 1], X_test[:, 2], y_test, c='g', marker='o')
+plot_y = sorted([i for i in regression.predict(X_train)])
+ax2.plot(sorted(X_train[:, 1]), sorted(X_train[:, 2]), plot_y, c="r")
+plt.title("Conjunto de Testeo")
+ax2.set_xlabel('R&D spend', fontsize=10)
+ax2.set_ylabel('Gastos en Marketing', fontsize=10)
+ax2.set_zlabel('Beneficios', fontsize=10)
+
 plt.show()
