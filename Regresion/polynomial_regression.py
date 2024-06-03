4a23a11d5114c1c531cdb2055d72a1cbfc879a58
TeoEchavarria <echavarria.0042@gmail.com>
2024-06-03 16:52:01 -0500

    Polynomial Regression

---
+++ Regresion/polynomial_regression.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from utils.data_processing import DataProcessing  # Assuming the class is in a module named data_processing
+
+class PolynomialRegression:
+    def __init__(self, data_path, degree=2):
+        self.data_processor = DataProcessing(data_path, test_size=0.2, random_state=0)
+        self.degree = degree
+        self.poly_model = LinearRegression()
+        self.poly_features = PolynomialFeatures(degree=self.degree)
+
+    def prepare_data(self):
+        self.data_processor.load_data(x_columns=slice(1,2), y_columns=2)
+        self.data_processor.split_data()
+
+    def train_model(self):
+        X_poly_train = self.poly_features.fit_transform(self.data_processor.X_train)
+        self.poly_model.fit(X_poly_train, self.data_processor.y_train)
+
+    def plot_results(self, title="Training Results", xlabel="Input Variable", ylabel="Output Variable"):
+        X_grid = np.arange(min(self.data_processor.X), max(self.data_processor.X), 0.1)
+        X_grid = X_grid.reshape(-1, 1)
+        X_grid_poly = self.poly_features.fit_transform(X_grid)
+        self.data_processor.plot_model(self.poly_model, title, xlabel, ylabel, X_grid_poly)
+
+    def predict(self, value):
+        value_poly = self.poly_features.fit_transform(np.array([[value]]))
+        return self.poly_model.predict(value_poly)
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 # Regresion Lineal
 from Regresion.simple_linear_regression import SimpleLinearRegressionModel
 from Regresion.multiple_linear_regression import MultipleLinearRegressionModel
+from Regresion.polynomial_regression import PolynomialRegression
 
 if __name__ == "__main__":
     # Linear Regression
-    # model = SimpleLinearRegressionModel('DataSets/Salary_Data.csv', x_columns=[0], y_column=1)
-    # model.train_model()
-    # model.plot_training_results("Sueldo vs Años de Experiencia (Conjunto de Entrenamiento)", "Años de Experiencia", "Sueldo (en USD$)")
-    # model.plot_testing_results("Sueldo vs Años de Experiencia (Conjunto de testing)", "Años de Experiencia", "Sueldo (en USD$)")
+    # linear_reg = SimpleLinearRegressionModel('DataSets/Salary_Data.csv', x_columns=[0], y_column=1)
+    # linear_reg.train_model()
+    # linear_reg.plot_training_results("Sueldo vs Años de Experiencia (Conjunto de Entrenamiento)", "Años de Experiencia", "Sueldo (en USD$)")
+    # linear_reg.plot_testing_results("Sueldo vs Años de Experiencia (Conjunto de testing)", "Años de Experiencia", "Sueldo (en USD$)")
     
     # Linear Multiple Regression
-    model = MultipleLinearRegressionModel('DataSets/50_Startups.csv', x_columns=[0,1,2,3], y_column=4, categorical_features_indices=[3])
-    model.train_model()
-    model_summary = model.backward_elimination_with_plot()
+    # linear_multiple_reg = MultipleLinearRegressionModel('DataSets/50_Startups.csv', x_columns=[0,1,2,3], y_column=4, categorical_features_indices=[3])
+    # linear_multiple_reg.train_model()
+    # linear_multiple_reg = model.backward_elimination_with_plot()
+    
+    # Polynomial Regression
+    poly_reg = PolynomialRegression('DataSets/Position_Salaries.csv', degree=4)
+    poly_reg.prepare_data()
+    poly_reg.train_model()
+    poly_reg.plot_results()
+
+    print(poly_reg.predict(6.5))
